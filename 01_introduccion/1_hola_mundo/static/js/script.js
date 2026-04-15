ccf59f2845ca0281df2e66531289196612a8dbf8
tomasmariqueo-VVH <tomasmariqueo@liceovvh.cl>
2026-04-15 08:09:59 -0400

    wasd

---
+++ 01_introduccion/1_hola_mundo/static/js/script.js
@@ -0,0 +1,13 @@
+// Prueba de conexión con js
+console.log("Hola Mundo, desde archivo externo!...");
+
+// 1) Hola mundo con variable y const
+const saludo = "Hola a todos, ";
+let destinatario = "estudiantes";
+// Impresión del saludo concatenando con (+).
+console.log(saludo + "para los " + destinatario);
+
+//2) Saludar a una persona ingresada por teclado.
+let nombre = prompt("Ingrese su nombre: ");
+//Imprimir un nombre ingresado con un prompt
+console.log("Hola, " + nombre);
---
+++ 02_tipos_de_datos/1-tipo_texto/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("Conexión con js correcta...");
+/*
+Dato: tipo texto (string)
+Concantenación de texto con el signo (+)
+Podemos unir texto y variables
+*/
+const nombre = "Juan";
+const apellido = "Pérez";
+//Unimos ambas constantes con  un texto extra.
+console.log("Hola, mi nombre es " + nombre + " " + apellido);
